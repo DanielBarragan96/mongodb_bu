1d7f89fe3c57a7145336cc411edb641b64bfce54
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 12:57:36 -0600

    backup command for obras

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -2,3 +2,4 @@ export $(grep -v '^#' .env | xargs)
 export DATE=$(date +'%Y-%m-%d')
 
 mongoexport --uri $URI/generaldb --collection clientes --type csv --out ./clientes/clientes_$DATE.csv --fields clienteMin,clienteString,notas
+mongoexport --uri $URI/generaldb --collection obras --type csv --out ./obras/obras_$DATE.csv --fields clienteMin,obraMin,obraString,iva,retIsr,retIva,notas
