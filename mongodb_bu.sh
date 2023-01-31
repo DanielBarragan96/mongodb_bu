1312cdab9b3cbc4092caf2fe18714669d7da7242
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 13:04:43 -0600

    backup command for fletes

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -4,3 +4,5 @@ export DATE=$(date +'%Y-%m-%d')
 mongoexport --uri $URI/generaldb --collection clientes --type csv --out ./clientes/clientes_$DATE.csv --fields clienteMin,clienteString,notas
 mongoexport --uri $URI/generaldb --collection obras --type csv --out ./obras/obras_$DATE.csv --fields clienteMin,obraMin,obraString,iva,retIsr,retIva,notas
 mongoexport --uri $URI/generaldb --collection equipos --type csv --out ./equipos/equipos_$DATE.csv --fields index,equipoMin,equipoString,precio,notas
+
+mongoexport --uri $URI/fletesdb --collection fletes --type csv --out ./fletes/fletes_$DATE.csv --fields folio,fecha,clienteMin,obraMin,es,precio,maniobra,varios,desmoldante,cepillo,notas,equipo
