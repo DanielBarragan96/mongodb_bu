a0cc48700783331307464a172325cfe14e4eded3
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 12:57:19 -0600

    backup command for clientes

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -1,2 +1,4 @@
 export $(grep -v '^#' .env | xargs)
 export DATE=$(date +'%Y-%m-%d')
+
+mongoexport --uri $URI/generaldb --collection clientes --type csv --out ./clientes/clientes_$DATE.csv --fields clienteMin,clienteString,notas
