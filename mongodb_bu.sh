39a19f0f598b508bc35a366c14d3bfdbbe019ff5
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 12:58:04 -0600

    backup command for equipos

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -3,3 +3,4 @@ export DATE=$(date +'%Y-%m-%d')
 
 mongoexport --uri $URI/generaldb --collection clientes --type csv --out ./clientes/clientes_$DATE.csv --fields clienteMin,clienteString,notas
 mongoexport --uri $URI/generaldb --collection obras --type csv --out ./obras/obras_$DATE.csv --fields clienteMin,obraMin,obraString,iva,retIsr,retIva,notas
+mongoexport --uri $URI/generaldb --collection equipos --type csv --out ./equipos/equipos_$DATE.csv --fields index,equipoMin,equipoString,precio,notas
