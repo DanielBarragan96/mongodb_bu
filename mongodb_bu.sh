d19295522e3bdc7ac1b52bebfe20d0068a5c2556
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 13:05:05 -0600

    backup command for en_obra

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -6,3 +6,5 @@ mongoexport --uri $URI/generaldb --collection obras --type csv --out ./obras/obr
 mongoexport --uri $URI/generaldb --collection equipos --type csv --out ./equipos/equipos_$DATE.csv --fields index,equipoMin,equipoString,precio,notas
 
 mongoexport --uri $URI/fletesdb --collection fletes --type csv --out ./fletes/fletes_$DATE.csv --fields folio,fecha,clienteMin,obraMin,es,precio,maniobra,varios,desmoldante,cepillo,notas,equipo
+
+mongoexport --uri $URI/enobrasdb --collection en_obras --type csv --out ./enobras/enobras_$DATE.csv --fields fecha,clienteMin,obraMin,equipo
