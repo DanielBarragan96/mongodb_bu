290983c81a40c7f31460d269ab620d22b4dafbc1
DanielBarragan96 <ie702554@iteso.mx>
2023-01-31 13:05:35 -0600

    added URI using .env variables

--- mongodb_bu.sh
+++ mongodb_bu.sh
@@ -1,4 +1,6 @@
 export $(grep -v '^#' .env | xargs)
+
+export URI=mongodb+srv://$MONGODB_USR:$MONGODB_PSW@$MONGODB_CLUSTER
 export DATE=$(date +'%Y-%m-%d')
 
 mongoexport --uri $URI/generaldb --collection clientes --type csv --out ./clientes/clientes_$DATE.csv --fields clienteMin,clienteString,notas
